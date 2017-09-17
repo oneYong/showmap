260e4ae198a9d28771cf205bbcadf50444dc610d
wonyong kim <wonyongkim@wonyongui-MacBook.local>
2017-09-17 15:49:27 +0900

    first commit!

---
+++ src/main/java/com/showmap/ShowmapApplication.java
@@ -0,0 +1,12 @@
+package com.showmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowmapApplication.class, args);
+	}
+}
